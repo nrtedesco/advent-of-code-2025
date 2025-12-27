c49bacda5294077a693c91b8e13a8dec1386ceac
Nicholas <nicholas.r.tedesco@gmail.com>
2025-12-27 11:51:48 -0500

    solves day 10 part 2

--- 10-factory.py
+++ 10-factory.py
@@ -9,7 +9,7 @@
 
 import argparse
 import time
-import numpy as np
+import z3
 
 parser = argparse.ArgumentParser() 
 parser.add_argument("-d", "--data", help="path to .txt file dataset for challenge")
@@ -28,101 +28,84 @@ print("\nStarting Script\n" + "---" * 40)
 
 ## strategy: use BFS to extend search one button press at a time. break when solution is found 
 
-# button_presses = 0 
-# for machine in manual: 
-#     # parse machine components (indicator lights + buttons) from line 
-#     desired_config  = machine.replace('[', '').replace(']', '').split()[0]
-#     raw_buttons = machine.split(']')[1].split('{')[0].replace('(', '').replace(')', '').split()
-#     buttons = []
-#     for button in raw_buttons: 
-#         buttons.append([int(v) for v in button.split(',')])
-
-#     # check edge case - starting position is desired position 
-#     start_config = ['.'] * len(desired_config)
-#     if (start_config == desired_config): 
-#         continue 
-
-#     # start BFS
-#     i = 0 
-#     paths = [start_config]
-#     solved = False
-
-#     while (not solved): 
-#         i += 1
-#         next_paths = [] 
-#         for path in paths: 
-#             for button in buttons: 
-#                 next_path = path[:]
-#                 for num in button: 
-#                     if (next_path[num] == '#'): 
-#                         next_path[num] = '.'
-#                     else: 
-#                         next_path[num] = '#'
-#                 if (''.join(next_path) == desired_config): 
-#                     solved = True 
-#                     break 
-#                 next_paths.append(next_path) 
-#             if (solved): 
-#                 break 
-#         paths = next_paths 
-    
-#     button_presses += i 
-
-# print(f"Puzzle 1: {button_presses}")
-
-
-# puzzle 2 ----------------------------------------------------------------------------------------
-
-## strategy: use BFS to extend search one button press at a time, but now search relative to joltage requirements 
-
-def check_joltage(curr_joltage:list[int], min_joltage:list[int]) -> bool: 
-
-    satisfied = True
-    for v1, v2 in zip(curr_joltage, min_joltage): 
-        if (v1 < v2): 
-            satisfied = False 
-
-    return satisfied 
-
-
 button_presses = 0 
 for machine in manual: 
-    # parse machine components (required joltage + buttons) from line 
-    min_joltage = np.array([int(v) for v in machine.split('{')[1].replace('}', '').split(',')])
-    buttons = [np.array([int(v) for v in b.split(',')]) for b in machine.split(']')[1].split('{')[0].replace('(', '').replace(')', '').split()]
+    # parse machine components (indicator lights + buttons) from line 
+    desired_config  = machine.replace('[', '').replace(']', '').split()[0]
+    raw_buttons = machine.split(']')[1].split('{')[0].replace('(', '').replace(')', '').split()
+    buttons = []
+    for button in raw_buttons: 
+        buttons.append([int(v) for v in button.split(',')])
 
-    # set default joltage + check against requirements 
-    start_joltage = [0] * len(min_joltage)
-    if (check_joltage(start_joltage, min_joltage)): 
+    # check edge case - starting position is desired position 
+    start_config = ['.'] * len(desired_config)
+    if (start_config == desired_config): 
         continue 
 
-    # start search 
+    # start BFS
     i = 0 
-    solved = False 
-    paths = [start_joltage] 
-    visited = set() 
+    paths = [start_config]
+    solved = False
 
     while (not solved): 
         i += 1
         next_paths = [] 
         for path in paths: 
             for button in buttons: 
-                next_path = path.copy()
+                next_path = path[:]
                 for num in button: 
-                    next_path[num] += 1
-                if (check_joltage(next_path, min_joltage)): 
-                    print("B")
+                    if (next_path[num] == '#'): 
+                        next_path[num] = '.'
+                    else: 
+                        next_path[num] = '#'
+                if (''.join(next_path) == desired_config): 
                     solved = True 
                     break 
-                if (''.join([str(v) for v in next_path]) not in visited): 
-                    next_paths.append(next_path) 
-                    visited.add(''.join([str(v) for v in next_path]))
+                next_paths.append(next_path) 
             if (solved): 
                 break 
-        paths = next_paths
-        print("i")
+        paths = next_paths 
+    
+    button_presses += i 
 
-    button_presses += i
+print(f"Puzzle 1: {button_presses}")
+
+
+# puzzle 2 ----------------------------------------------------------------------------------------
+
+## NOTE: wasn't able to figure this one out on my own, so used reddit + Z3 documentation to help me through it
+## - source: https://www.reddit.com/answers/6c6648fb-00d8-4665-8c3a-621dc31b89ee/?q=Advent+of+Code+2025+Day+10+solutions&source=PDP&tl=en
+
+button_presses = 0
+for machine in manual: 
+
+    # parse out machine components (joltage + buttons) 
+    min_joltage = [int(v) for v in machine.split('{')[1].replace('}', '').split(',')]
+    buttons = [[int(v) for v in b.split(',')] for b in machine.split(']')[1].split('{')[0].replace('(', '').replace(')', '').split()]
+
+    # convert components to Z3 variables + optimization problem 
+
+    optimizer = z3.Optimize()
+    vars = [z3.Int(f"x{i}") for i in range(len(buttons))]
+
+    ## constraint 1: all button counters should be at least 0 
+    for var in vars: 
+        optimizer.add(var >= 0)
+
+    ## constraint 2: each respective button 
+    for i in range(len(min_joltage)): 
+        optimizer.add(sum(1 * vars[j] if i in buttons[j] else 0 * vars[j] for j in range(len(vars))) == min_joltage[i])
+
+    ## minimize total button presses 
+    t = z3.Int('t')
+    optimizer.add(t == sum(vars)) 
+    optimizer.minimize(t)
+    
+    if optimizer.check() == z3.sat:
+        model = optimizer.model()
+        button_presses += model.eval(t).as_long()
+    else:
+        raise Exception("no solution found")
 
 print(f"Puzzle 2: {button_presses}")
 
